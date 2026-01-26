da0bc53088a4da1b2700482618e759bc5e613cb8
dhiaa-alomari <dhiaaalomari81@gmail.com>
2026-01-26 04:13:16 +0000

    add paths in urls.py in pizzatasty project and content to apps.py in menu app

--- pizzatasty/urls.py
+++ pizzatasty/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('menu.urls')),
+    path("accounts/", include("allauth.urls")),
+    path(
+        'login/', auth_views.LoginView.as_view(
+            template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('booking/', include('booking.urls')),
 ]
