d8d3d7f22dd315bbaafa9f400d5b7741fa046f0e
dhiaa-alomari <dhiaaalomari81@gmail.com>
2026-01-26 03:56:34 +0000

    add schema in file models.py in menu directory

--- menu/models.py
+++ menu/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
-# image = CloudinaryField(
-#         'image',
-#         default='placeholder', null=True, blank=True)
+class Menu(models.Model):
+    """
+    Represents a menu item in the restaurant, including name,
+    description, price, and an optional image field.
+    """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = CloudinaryField(
+        'image',
+        default='placeholder', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
