7a4a65c07a732937c999af40f673a67b08e7da0b
dhiaa-alomari <dhiaaalomari81@gmail.com>
2026-01-26 04:03:36 +0000

    add schema urls path for booking application

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Table(models.Model):
+    """
+    Model representing a table with a specific capacity.
+    Orders tables by descending capacity.
+    """
+    capacity = models.IntegerField()
+
+    class Meta:
+        ordering = ('-capacity',)
+
+    def __str__(self):
+        return f"Table Capacity: {self.capacity}"
+
+
+class Booking(models.Model):
+    """
+    Model representing a booking.
+    Includes fields for user, table, date, time, and number of guests.
+    """
+    GUESTS_CHOICES = [(1, 1), (2, 2), (3, 3), (4, 4),
+                      (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)]
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='bookings')
+    table = models.ForeignKey(
+        Table, on_delete=models.CASCADE, related_name='bookings')
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField(
+        choices=GUESTS_CHOICES,
+        default=1,
+        validators=[
+         MaxValueValidator(10),
+         MinValueValidator(1)
+        ]
+    )
+
+    def __str__(self):
+        return f"Booking for {self.user.username.capitalize()} on {
+            self.date} at {self.time} for {self.guests} guests"
---
+++ booking/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # auth
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
+    # bookings
+    path('book-table/', views.book_table, name='book-table'),
+    path('user-bookings/', views.user_bookings, name='user-bookings'),
+    path('get-booking/<int:id>/', views.get_booking, name='get-booking'),
+    path(
+        'update-booking/<int:id>/', views.update_booking, name='update-booking'
+        ),
+    path(
+        'cancel-booking/<int:id>/',
+        views.cancel_booking,
+        name='cancel-booking'),
+    ]
