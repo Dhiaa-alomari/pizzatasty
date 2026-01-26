e68813d8574ea34882bcd42155bcba303b88705b
dhiaa-alomari <dhiaaalomari81@gmail.com>
2026-01-26 04:16:04 +0000

    create html files in templates directory for menu app

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Menu
 
-# Create your views here.
+
+def home_view(request):
+    return render(request, 'menu/home.html')
+
+
+def menu_view(request):
+    menu = Menu.objects.all()
+    context = {
+        'menu_list': menu
+    }
+    return render(request, 'menu/menu.html', context)
